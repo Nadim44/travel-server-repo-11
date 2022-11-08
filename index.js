cb9b4b214c58a3846385a55eef20b0a6fbb2c915
2Nadim <nadimmahamud661@gmail.com>
2022-11-08 05:06:15 +0000

    load data from mongobd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -15,12 +15,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vtfbuf5.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        const placeCollection = client.db('travelBD').collection('places');
+
+        app.get('/places', async (req, res) => {
+            const query = {};
+            const cursor = placeCollection.find(query)
+            const places = await cursor.toArray();
+            res.send(places)
+        })
+
+        app.get('/places/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const place = await placeCollection.findOne(query)
+            res.send(place)
+        })
+
+    }
+    finally {
+
+    }
+
+
+}
+
+run().catch(err => console.error(err))
 
 
 
